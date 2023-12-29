be15a9183688fca9295388c1b0c3b83a82899480
SuperSection <soumosarkar297@gmail.com>
2023-12-30 02:39:21 +0530

    add User Login API

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,6 +3,7 @@ import cors from "cors";
 import "dotenv/config";
 import mongoose from "mongoose";
 import userRoutes from "./routes/users.routes";
+import authRoutes from "./routes/auth.routes";
 
 mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string);
 
@@ -12,7 +13,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
+
+// Declare Routes
+app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
+
 
 app.listen(7000, () => {
   console.log("Server is running on PORT: 7000");
---
+++ backend/src/routes/auth.routes.ts
@@ -0,0 +1,59 @@
+import express, { Request, Response } from "express";
+import { check, validationResult } from "express-validator";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import User from "../models/user.model";
+
+const router = express.Router();
+
+router.post(
+  "/login",
+  [
+    check("email", "Email is required.").isEmail(),
+    check("password", "Password with 6 or more characters required.").isLength({
+      min: 6,
+    }),
+  ],
+  async (req: Request, res: Response) => {
+    const erros = validationResult(req);
+    if (!erros.isEmpty()) {
+      return res.status(400).json({ message: erros.array() });
+    }
+
+    const { email, password } = req.body;
+
+    try {
+      const user = await User.findOne({ email });
+      if (!user) {
+        return res.status(400).json({ message: "User not found." });
+      }
+
+      const isPasswordCorrect = await bcrypt.compare(password, user.password);
+      if (!isPasswordCorrect) {
+        return res.status(400).json({ message: "Invalid password." });
+      }
+
+      const token = jwt.sign(
+        { userId: user.id },
+        process.env.JWT_SECRET_KEY as string,
+        {
+          expiresIn: "1d",
+        }
+      );
+
+      res.cookie("auth_token", token, {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === "production",
+        maxAge: 1000 * 60 * 60 * 24,
+      });
+
+      res.status(200).json({ userId: user._id });
+      
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({ message: "Something went wrong." });
+    }
+  }
+);
+
+export default router;
