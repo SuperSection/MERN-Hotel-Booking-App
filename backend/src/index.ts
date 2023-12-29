1a1f372c72bdb44f0d4d20adc3599e8a4e74263b
SuperSection <soumosarkar297@gmail.com>
2023-12-29 21:27:06 +0530

    add User Register API

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,8 @@
-import express, { Request, Response } from "express";
+import express from "express";
 import cors from "cors";
 import "dotenv/config";
 import mongoose from "mongoose";
+import userRoutes from "./routes/users.routes";
 
 mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string);
 
@@ -10,9 +11,8 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
-app.get("/api/test", async (req: Request, res: Response) => {
-  res.json({ message: "Checking the server!" });
-});
+
+app.use("/api/users", userRoutes);
 
 app.listen(7000, () => {
   console.log("Server is running on PORT: 7000");
